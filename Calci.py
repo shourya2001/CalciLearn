0443e95e615982629338155119e0dd7907110369
SaloniPatel <saloni.patel01@gmail.com>
2021-01-29 19:04:08 +0530

    division

--- Calci.py
+++ Calci.py
@@ -10,16 +10,16 @@ def multiply(x, y):
     return x * y
 
 # This function divides two numbers
-#def divide(x, y):
-   # return x / y
+def divide(x, y):
+    return x / y
 
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
-#print("4.Divide")
+print("4.Divide")
 
 while True:
     # Take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
