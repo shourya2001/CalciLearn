6bfc01c46b326dd3660b22e57057a41b8a904d88
SaloniPatel <saloni.patel01@gmail.com>
2021-01-29 18:57:38 +0530

    multi

--- Calci.py
+++ Calci.py
@@ -6,20 +6,20 @@ def subtract(x, y):
     return x - y
 
 # This function multiplies two numbers
-#def multiply(x, y):
-   # return x * y
+def multiply(x, y):
+    return x * y
 
 # This function divides two numbers
 #def divide(x, y):
-  #  return x / y
+   # return x / y
 
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
-#print("3.Multiply")
+print("3.Multiply")
 #print("4.Divide")
 
 while True:
     # Take input from the user
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/3): ")
